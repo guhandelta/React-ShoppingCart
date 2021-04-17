c82c4d65309cfa7c1f79a418b7360317dead2f1e
Kala Foods <guhan.delta@gmail.com>
2021-04-17 15:43:44 -0500

    Provided appropriate styling for the product info

--- src/components/CartItem/CartItem.styles.ts
+++ src/components/CartItem/CartItem.styles.ts
@@ -1,3 +1,27 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    width: 100%;
+    border: 0.0625em solid lightblue;
+    border-radius: 1.25em;
+    height: 100%;
+
+    button {
+        border-radius: 0 0 1.25rem 1.25rem
+    }
+
+    img{
+        max-height: 250px;
+        object-fit: cover;
+        border-radius: 1.25rem 1.25rem 0 0;
+    }
+
+    div{
+        font-family: Arial, Helvetica, sans-serif;
+        padding: 1rem;
+        height: 100%;
+    }
+`
