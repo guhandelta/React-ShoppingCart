28b1afd7d218dbd2f01943ac1986ea00632cebc1
Kala Foods <guhan.delta@gmail.com>
2021-04-16 23:04:07 -0500

    Wrapped the App.tsx with ReactQuery's QueryClientProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom'
 import App from './App'
+import { QueryClient, QueryClientProvider } from "react-query";
 
-render(<App />, document.getElementById('root'));
+const client = new QueryClient();
+
+render( 
+        <QueryClientProvider client={client}>
+            <App />
+        </QueryClientProvider>,
+        document.getElementById('root')
+      );
